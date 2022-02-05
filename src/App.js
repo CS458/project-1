0bd2ceae4ccae7fb2c09ca24cc51ed32a98e123a
Osama Tanveer <osama.tanveer@ug.bilkent.edu.tr>
2022-02-05 22:36:06 +0500

    Implment Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux";
+import { Provider } from "react-redux";
 
 import { RoutesContainer } from "./routes/RoutesContainer";
 
 const App = () => {
 	return (
-		<div className='App'>
-			<BrowserRouter>
-				<RoutesContainer />
-			</BrowserRouter>
-		</div>
+		<Provider store={store}>
+			<div className='App'>
+				<BrowserRouter>
+					<RoutesContainer />
+				</BrowserRouter>
+			</div>
+		</Provider>
 	);
 };
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../../redux/counter/counterSlice";
 import css from "./Home.module.css";
 
 export const Home = () => {
+	const count = useSelector((state) => state.counter.value);
+	const dispatch = useDispatch();
+
 	return (
 		<div className={css.container}>
-			<div>Home</div>
+			<button aria-label='Increment value' onClick={() => dispatch(increment())}>
+				Increment
+			</button>
+			<span>{count}</span>
+			<button aria-label='Decrement value' onClick={() => dispatch(decrement())}>
+				Decrement
+			</button>
 		</div>
 	);
 };
